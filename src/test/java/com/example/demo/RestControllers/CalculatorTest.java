87e10d5506254be2c837a8f2732258e7019d76ed
trasen <felixzetterlund@gmail.com>
2019-10-21 19:19:35 +0200

    First commit, late creation of repo.

---
+++ src/test/java/com/example/demo/RestControllers/CalculatorTest.java
@@ -0,0 +1,57 @@
+package com.example.demo.RestControllers;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+
+    @Test
+    public void testCalculatorSingleAdditions() {
+
+        assertEquals("20",calculator.calculator("5+5+5+5"));
+    }
+
+    @Test
+    public void testCalculatorMultiAdditions() {
+
+        assertEquals("200", calculator.calculator("50+50+50+50"));
+
+    }
+
+    @Test
+    public void testCalculatorDecimalAddition() {
+        assertEquals("4.5", calculator.calculator("4 + 0.5"));
+    }
+
+    @Test
+    public void testCalculatorMultiplication()  {
+        assertEquals("200", calculator.calculator("50*4"));
+    }
+
+    @Test
+    public void testCalculatorMultiplicationDecimal() {
+        assertEquals("1000.5", calculator.calculator("500.25 * 2"));
+    }
+
+    @Test
+    public void testCalculatorMultiplicationMultiValues() {
+        assertEquals("100", calculator.calculator("5*5*2*2"));
+    }
+
+    @Test
+    public void testCalculatorMultiplicationAndAddition() {
+        assertEquals("75", calculator.calculator("25+25*2"));
+    }
+
+    @Test
+    public void testCalculatorMultiplicationAndAdditionMulti() {
+        assertEquals("205", calculator.calculator("50+50+25+10*2*4"));
+    }
+}
